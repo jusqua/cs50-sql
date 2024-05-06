50d6eea9d79f20e1d127e71e2a19073f7e7206d7
Ádrian Gama <adrian.gama.se@gmail.com>
2024-05-06 16:29:12 -0300

    Add Week 0 Cyberchase Solution Fix #1

---
+++ week0/cyberchase/12.sql
@@ -0,0 +1,1 @@
+SELECT COUNT(DISTINCT "title") AS "number of episodes with different title" FROM "episodes";
---
+++ week0/cyberchase/13.sql
@@ -0,0 +1,1 @@
+SELECT "title" FROM "episodes" WHERE ("topic" LIKE '%measurement%' OR "topic" LIKE '%fraction%') AND ("season" BETWEEN 2 AND 6);
